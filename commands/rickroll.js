bf28c93fdf201b25d3d430870527562338910ccb
Monke But Cool <110347394+monkebutcool@users.noreply.github.com>
2023-11-28 14:14:08 +0300

    Create rickroll.js

---
+++ commands/rickroll.js
@@ -0,0 +1,7 @@
+module.exports = [{
+  name: "curl ascii.live/can-you-hear-me", 
+  aliases: ["curl ascii.live/rick"], 
+  nonPrefixed: true, 
+  code: `$description[
+  $color[00000;00000]
+https://cdn.discordapp.com/attachments/1144646461607395461/1179016622992793631/rickroll.gif?ex=65783fcb&is=6565cacb&hm=9ed4ea24e53078314cb238d4d83ee2a313568ffa8ab788fe54af64ce82583577&`}]
