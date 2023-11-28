d567d473ba487ca1271f381eb388a296903d7797
Monke But Cool <110347394+monkebutcool@users.noreply.github.com>
2023-11-28 14:15:27 +0300

    Create parrot.js

---
+++ commands/parrot.js
@@ -0,0 +1,7 @@
+module.exports = [{
+  name: "curl ascii.live/parrot", 
+  aliases: ["curl parrot.live"], 
+  nonPrefixed: true, 
+  code: `$description[
+  $color[00000;00000]
+https://cdn.discordapp.com/attachments/1144646461607395461/1179016622619508867/parrot.gif?ex=65783fcb&is=6565cacb&hm=8390b431ce80b8ad11fd9cdebee17d8f30240c15b54ee02cd690f3f83c36f3d7&`}]
